9ef63d0dc98f0008ec678cb56c8492e71c3ae82b
jkh0kr <admin@jkh.kr>
2026-01-08 14:55:25 +0900

    Update settings.py

--- rdgen/settings.py
+++ rdgen/settings.py
@@ -32,7 +32,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 #CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split()
